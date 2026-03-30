6683105da2773eded6f608e66cdb3b34d2b919ce
amuhaimen <amuhaimenpro829@gmail.com>
2026-03-30 17:16:37 +0600

    leads history in secretary role done

--- app/(secretary)/secretary-dashboard/lead-history/leads/page.tsx
+++ app/(secretary)/secretary-dashboard/lead-history/leads/page.tsx
@@ -1,7 +1,8 @@
+import LeadsHome from '@/components/secretary-dashboard/lead-history/LeadsHome'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <LeadsHome/>
   )
 }
