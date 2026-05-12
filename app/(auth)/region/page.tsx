ce1e87b6d6bf498edf4b00ace240d6cbe83debf0
SujonRoyBD <sujonroy537527@gmail.com>
2026-05-12 15:20:24 +0600

    next

--- app/(auth)/region/page.tsx
+++ app/(auth)/region/page.tsx
@@ -1,12 +1,16 @@
+import UserResponsesPage from "@/app/(admin)/dashboard/connection/status/page";
 import Login from "@/components/auth/Login";
 import Rigion from "@/components/auth/Rigion";
+import { Suspense } from "react";
 
 
 export default function page() {
   return (
     <div>
       {/* <Rigion/> */}
-      <Login />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Login />
+      </Suspense>
     </div>
   )
 }
